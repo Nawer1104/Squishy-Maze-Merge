f15eebe16e056495f952c3a3743cbbd3790f96df
SteveNguyen <nam.ngh2211@mail.com>
2024-04-13 16:49:30 +0700

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject vfxOnKill;
+    public GameObject vfxOnDead;
+
+    private Vector3 startPos;
+
+    private void Awake()
+    {
+        startPos = transform.position;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Target")
+        {
+            GameObject vfx = Instantiate(vfxOnKill, collision.gameObject.transform.position, Quaternion.identity);
+            Destroy(vfx, 1f);
+
+            GameManager.Instance.levels[GameManager.Instance.GetCurrentIndex()].gameObjects.Remove(collision.gameObject);
+            collision.gameObject.SetActive(false);
+
+            GameManager.Instance.CheckLevelUp();
+        }
+        else if (collision.gameObject.tag == "wall")
+        {
+            ReSetPos();
+        }
+    }
+
+
+    public void ReSetPos()
+    {
+        transform.position = startPos;
+        GameObject vfx = Instantiate(vfxOnDead, transform.position, Quaternion.identity);
+        Destroy(vfx, 1f);
+
+    }
+}
